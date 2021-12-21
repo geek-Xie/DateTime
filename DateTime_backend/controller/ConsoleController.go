2947721c4bda49a1598ff8b80827ff2d201098fb
xiexunchao <504428538@qq.com>
2021-12-21 11:15:28 +0800

    eventitem界面现实事件细节

--- DateTime_backend/controller/ConsoleController.go
+++ DateTime_backend/controller/ConsoleController.go
@@ -15,6 +15,14 @@ type CtxInfo struct {
 	UserEmail string `json:"userInfo"`
 }
 
+type EventItem struct {
+	Title       string
+	Description string
+	StartDate   string
+	StartTime   string
+	EndTime     string
+}
+
 func Console(c *gin.Context) {
 	var requestMap = make(map[string]string)
 	json.NewDecoder(c.Request.Body).Decode(&requestMap)
@@ -24,8 +32,19 @@ func Console(c *gin.Context) {
 	//log.Println(userEmail)
 	log.Println(time.Now())
 	events := server.GetEvents(userEmail, activeYMD)
+	var eventItems []EventItem
+	for i := 0; i < len(events); i++ {
+		eventitem := EventItem{
+			Title:       events[i].Title,
+			Description: events[i].Description,
+			StartDate:   events[i].StartDate,
+			StartTime:   events[i].StartTime,
+			EndTime:     events[i].EndTime,
+		}
+		eventItems = append(eventItems, eventitem)
+	}
 	if len(events) != 0 {
-		response.Response(c, http.StatusOK, 200, gin.H{"events": events}, "该日期有事件记录")
+		response.Response(c, http.StatusOK, 200, gin.H{"eventItems": eventItems}, "该日期有事件记录")
 	} else {
 		response.Response(c, http.StatusOK, 400, nil, "该日期无事件记录")
 	}
--- datetime_web/src/components/EventItems.vue
+++ datetime_web/src/components/EventItems.vue
@@ -9,22 +9,41 @@ export default {
   data() {
     return {
       // Note `isActive` is left out and will not appear in the rendered table
-      fields: ["first_name", "last_name", "age"],
+      fields: ["Title", "Description", "StartDate", "StartTime", "EndTime"],
+      item: {
+        Title: "",
+        Description: "",
+        StartDate: "",
+        StartTime: "",
+        EndTime: "",
+      },
       items: [
-        {
-          isActive: true,
-          age: 40,
-          first_name: "Dickerson",
-          last_name: "Macdonald",
-        },
-        { isActive: false, age: 21, first_name: "Larsen", last_name: "Shaw" },
-        { isActive: false, age: 89, first_name: "Geneva", last_name: "Wilson" },
-        { isActive: true, age: 38, first_name: "Jami", last_name: "Carney" },
+        // {
+        //   isActive: true,
+        //   age: 40,
+        //   first_name: "Dickerson",
+        //   last_name: "Macdonald",
+        // },
+        // { isActive: false, age: 21, first_name: "Larsen", last_name: "Shaw" },
+        // { isActive: false, age: 89, first_name: "Geneva", last_name: "Wilson" },
+        // { isActive: true, age: 38, first_name: "Jami", last_name: "Carney" },
       ],
     };
   },
   created() {
-    console.log(this.$route.query.data);
+    // for (let i = 0; i < this.$route.query.data.length; i++) {
+    //   console.log(this.$route.query.data[i]);
+    // }
+    for (var i = 0; i < this.$route.query.data["eventItems"].length; i++) {
+      let item = {
+        Title: this.$route.query.data["eventItems"][i]["Title"],
+        Description: this.$route.query.data["eventItems"][i]["Description"],
+        StartDate: this.$route.query.data["eventItems"][i]["StartDate"],
+        StartTime: this.$route.query.data["eventItems"][i]["StartTime"],
+        EndTime: this.$route.query.data["eventItems"][i]["EndTime"],
+      };
+      this.items[i] = item;
+    }
   },
 };
 </script>
